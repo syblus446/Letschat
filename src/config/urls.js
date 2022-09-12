b602d17078c972f711d8bbafb5d26d02a82e6236
Nilay2006 <nilayfreefire2006@gmail.com>
2022-09-12 21:05:10 +0530

    letschat

---
+++ src/config/urls.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = 'http://10.0.2.2:3000';
+
+export const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
+
+export const SIGNUP_API = getApiUrl('/User/signup');
+export const GET_USERS = getApiUrl('/User/users');
+export const VERIFY_OTP = getApiUrl('/User/otpVerify');
